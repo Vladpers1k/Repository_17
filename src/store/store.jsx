b83d731300c8dff3eeaae237045c42c316cb8962
Vladpers1k <vladik340123@gmail.com>
2025-01-12 11:46:49 +0200

    created store folder and created fiels in it

---
+++ src/store/store.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import usersReducer from './usersSlice'
+import settingsReducer from './settingsSlice'
+
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    settings: settingsReducer
+  }
+})
